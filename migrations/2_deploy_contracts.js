28b626c4383672d517a35fedf60c9ddd7bc18d66
Mike Piehl <piehlm@paperstreetenterprises.com>
2021-06-13 08:21:37 -0400

    built front end

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var MDPTokenSale = artifacts.require("./MDPTokenSale.sol");
 
 module.exports = function(deployer) {
 	deployer.deploy(MDPToken, 1000000).then(function() {
-		var tokenPrice = 1000000000000;
+		var tokenPrice = 100000000000000;
 		return deployer.deploy(MDPTokenSale, MDPToken.address, tokenPrice);
 	});
 	
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,133 @@
-console.log("app.js loaded")
+App = {
+	web3Provider: null,
+	contracts: {},
+	account: '0x0',
+	loading: false,
+	tokenPrice: 100000000000000,
+	tokenSold: 0,
+	tokensAvailable: 750000,
+
+	init: function() {
+		console.log("App initialized...")
+		return App.initWeb3();
+	},
+
+	initWeb3: function() {
+		if (typeof web3 !== 'undefined') {
+			// If a web3 instance is already provided by Meta Mask
+			App.web3Provider = ethereum;
+			web3 = new Web3(ethereum);
+		} else {
+			// specify default instance if no web3 instance provided
+			App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+			web3 = new Web3(App.web3Provider);
+		}
+		App.initContracts();
+	},
+
+	initContracts: function() {
+		$.getJSON("MDPTokenSale.json", function(mdpTokenSale) {
+			App.contracts.MDPTokenSale = TruffleContract(mdpTokenSale);
+			App.contracts.MDPTokenSale.setProvider(App.web3Provider);
+			App.contracts.MDPTokenSale.deployed().then(function(mdpTokenSale) {
+				console.log("MDP Token Sale Address:", mdpTokenSale.address);
+			});
+		}).done(function() {
+			$.getJSON("MDPToken.json", function(mdpToken) {
+				App.contracts.MDPToken = TruffleContract(mdpToken);
+				App.contracts.MDPToken.setProvider(App.web3Provider);
+				App.contracts.MDPToken.deployed().then(function(mdpToken) {
+					console.log("MDP Token Address:", mdpToken.address);
+				});
+				App.listenForEvents();
+				return App.render();
+			});
+		})
+	},
+
+	//listen for events submitted from the contract
+	listenForEvents: function() {
+		App.contracts.MDPTokenSale.deployed().then(function(instance) {
+			instance.Sell({}, {
+				fromBlock: 0,
+				toBlock: 'latest',
+			}).watch(function(error, event) {
+				console.log("event triggered", event);
+				App.render();
+			})
+		})
+	},
+
+	render: function() {
+		if(App.loading){
+			return;
+		}
+		App.loading = true;
+
+		var loader = $('#loader');
+		var content = $('#content');
+
+		loader.show();
+		content.hide();
+
+		//load account data
+	    web3.eth.getCoinbase(function(err, account) {
+	      if(err === null) {
+	        App.account = account;
+	        $('#accountAddress').html("Your Account: " + account);
+	      }
+	    })
+		// load token sale contract
+		App.contracts.MDPTokenSale.deployed().then(function(instance) {
+			mdpTokenSaleInstance = instance;
+			return mdpTokenSaleInstance.tokenPrice();
+		}).then(function(tokenPrice) {
+			App.tokenPrice = tokenPrice;
+			$('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+			return mdpTokenSaleInstance.tokensSold();
+		}).then(function(tokensSold) {
+			App.tokensSold = tokensSold.toNumber();
+			$('.tokens-sold').html(App.tokensSold);
+			$('.tokens-available').html(App.tokensAvailable);
+
+			var progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+			$('#progress').css('width', progressPercent + '%');
+
+			//load token contract
+			App.contracts.MDPToken.deployed().then(function(instance) {
+				mdpTokenInstance = instance;
+				return mdpTokenInstance.balanceOf(App.account);
+			}).then(function(balance) {
+				$('.mdp-balance').html(balance.toNumber());
+				App.loading = false;
+				loader.hide();
+				content.show();
+			})
+		})
+
+	},
+
+	buyTokens: function() {
+		$('#content').hide();
+		$('#loader').show();
+		var numberOfTokens = $('#numberOfTokens').val();
+		App.contracts.MDPTokenSale.deployed().then(function(instance) {
+			console.log(App.account);
+			return instance.buyTokens(numberOfTokens, { 
+				from: App.account,
+				value: numberOfTokens * App.tokenPrice,
+				gas: 500000
+			});
+		}).then(function(result) {
+			console.log("Tokens Bought...")
+			$('form').trigger('reset'); // reset number of tokens in the form
+			// wait for sell event
+		})
+	}
+}
+
+$(function() {
+	$(window).load(function() {
+		App.init();
+	})
+});
